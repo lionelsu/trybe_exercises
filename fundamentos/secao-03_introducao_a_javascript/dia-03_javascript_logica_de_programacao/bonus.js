c1168d02ae368fec63e348b6189e1fa843df2230
Lionel Miranda <bibliothecalionel@gmail.com>
2023-01-26 13:39:58 -0400

    exercicios dia 03

---
+++ fundamentos/secao-03_introducao_a_javascript/dia-03_javascript_logica_de_programacao/bonus.js
@@ -0,0 +1,19 @@
+let n = 9
+let simbolo = '*'
+let linha = ''
+
+for(let i = 0; i < n; i++) {
+    linha = linha + simbolo
+}
+
+for(let i = 0; i < n; i++) {
+    console.log(linha)
+}
+
+console.log(linha)
+
+/*
+for (let i = 0; i < n; i++) {
+    console.log(retangulo)
+    retangulo = retangulo + simbolo
+}*/

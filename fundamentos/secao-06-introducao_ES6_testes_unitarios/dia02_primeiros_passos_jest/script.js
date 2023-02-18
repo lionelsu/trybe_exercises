110eb2d99b7f70ec49378f36017cadf99ace2733
Lionel Miranda <bibliothecalionel@gmail.com>
2023-02-18 11:50:41 -0400

    lesson 1

---
+++ fundamentos/secao-06-introducao_ES6_testes_unitarios/dia02_primeiros_passos_jest/script.js
@@ -0,0 +1,15 @@
+// Exercício 1
+function myRemove(arr, item) {
+    let newArr = []
+    for (let index = 0; index < arr.length; index += 1) {
+        if (item !== arr[index]) {
+            newArr.push(arr[index])
+        }
+    }
+    return newArr
+}
+
+// Exercício 2
+
+// exportando
+module.exports = { myRemove };

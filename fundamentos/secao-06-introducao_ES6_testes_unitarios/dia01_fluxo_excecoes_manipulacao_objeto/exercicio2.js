a918c8c09a73771d27e9e5b2d6d4bb79937c85c4
lionelsu <lionel_suicune@hotmail.com>
2023-02-22 18:49:49 -0400

    some lessons

--- fundamentos/secao-06-introducao_ES6_testes_unitarios/dia01_fluxo_excecoes_manipulacao_objeto/exercicio2.js
+++ fundamentos/secao-06-introducao_ES6_testes_unitarios/dia01_fluxo_excecoes_manipulacao_objeto/exercicio2.js
@@ -17,3 +17,18 @@ const lesson3 = {
     professor: 'Maria Clara',
     turno: 'noite'
 }
+
+// Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+function addTurn (obj, key, value) {
+    obj[key] = value
+    return obj
+}
+
+console.log(addTurn(lesson2, 'turno', 'noite'))
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+function listObjKeys(obj) {
+    return Object.keys(obj)
+}
+console.log(listObjKeys(lesson1))
+

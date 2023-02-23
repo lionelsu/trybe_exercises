7907a3ac38b06321b92fa1114dfe82e80acc9325
lionelsu <lionel_suicune@hotmail.com>
2023-02-22 20:37:41 -0400

    terminando o bloco 3 de exercicios do dia 1

--- fundamentos/secao-06-introducao_ES6_testes_unitarios/dia01_fluxo_excecoes_manipulacao_objeto/exercicio2.js
+++ fundamentos/secao-06-introducao_ES6_testes_unitarios/dia01_fluxo_excecoes_manipulacao_objeto/exercicio2.js
@@ -59,3 +59,54 @@ function allLessonsD() {
 console.log(allLessonsD())
 
 // Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
+function getStudentsTotal(obj) {
+    let count = 0
+
+    const students = Object.keys(obj)
+    for (const student in students) {
+        count += obj[students[student]].numeroEstudantes
+    }
+    return count
+}
+console.log(getStudentsTotal(allLessonsD()))
+
+// Mesma coisa mas usando destructuring
+function getStudentsTotalD(obj) {
+    let count = 0
+
+    const students = Object.keys(obj)
+    // aqui utilizamos o for of
+    for (const student of students) {
+        const { numeroEstudantes } = obj[student]
+        count += numeroEstudantes
+    }
+    return count
+}
+console.log(getStudentsTotalD(allLessonsD()))
+
+
+// Mesma coisa com reduce
+function getStudentsTotalDR(obj) {
+    return Object.values(obj).reduce((total, { numeroEstudantes }) => { return total + numeroEstudantes }, 0)
+}
+console.log(getStudentsTotalDR(allLessonsD()))
+
+
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+function getKeyByNumber(obj, number) {
+    return Object.values(obj)[number]
+}
+console.log(getKeyByNumber(lesson1, 3))
+
+
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles o objeto, o nome da chave e o valor da chave.
+function objectPair(obj, key, value) {
+    for (const [chave, valor] of Object.entries(obj)) {
+        if (chave === key && valor === value) {
+            return true
+        }
+    }
+    return false
+}
+console.log(objectPair(lesson3, 'turno', 'noite'))
+console.log(objectPair(lesson3, 'materia', 'Maria Clara'))
